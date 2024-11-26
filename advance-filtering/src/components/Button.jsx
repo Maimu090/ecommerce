e5a114785304382ec32bd1155019990812d2e7ae
Maimuna <maimunaibrahim808@gmail.com>
2024-11-26 09:06:43 +0100

    new

--- advance-filtering/src/components/Button.jsx
+++ advance-filtering/src/components/Button.jsx
@@ -1,6 +1,10 @@
-function Button() {
-    return <div>
-Button
-    </div>
-}
-export default Button;
+const Button = ({ onClickHandler, value, title }) => {
+    return (
+      <button onClick={onClickHandler} value={value} className="btns">
+        {title}
+      </button>
+    );
+  };
+  
+  export default Button;
+  
